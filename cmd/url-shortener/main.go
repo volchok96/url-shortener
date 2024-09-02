3e21196d9e99d05033983bba4c02a4c057945119
Katerina Zakharova <kzakharova96@yandex.com>
2024-09-02 20:27:24 +0300

    added logging system with slog

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -2,10 +2,45 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
+	"os"
 	"url-shortener/internal/config"
+)
+
+const  (
+	envLocal = "local"
+	envDev = "dev"
+	envProd = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
 	fmt.Println(cfg)
+
+	log := setupLogger(cfg.Env)
+
+	log = log.With(slog.String("env", cfg.Env))
+
+	log.Info("starting url-shortener")
+	log.Debug("debug msg enabled")
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level:slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level:slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level:slog.LevelInfo}),
+		)
+		
+	}
+	return log
 }
