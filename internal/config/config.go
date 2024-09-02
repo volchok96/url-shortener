36f46a3b565450f987d3f98b3f9efe50306b9176
Katerina Zakharova <kzakharova96@yandex.com>
2024-09-02 19:43:53 +0300

    implemented main function; set environment variables

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,24 +3,31 @@ package config
 import (
 	"log"
 	"os"
+	"path/filepath"
 	"time"
+
 	"github.com/ilyakaznacheev/cleanenv"
 )
 
 type Config struct {
-	Env string `yaml:"env" env-default:"local" env-required:"true"`
+	Env         string `yaml:"env" env-default:"local" env-required:"true"`
 	StoragePath string `yaml:"storage_path" env-required:"true"`
-	HTTPServer `yaml:"http_server`
+	HTTPServer  `yaml:"http_server"`
 }
 
 type HTTPServer struct {
-	Address string `yaml:"address" env-default:"localhost:8080"`
-	Timeout time.Duration `yaml:"timeout" env-default:"4s"`
+	Address     string        `yaml:"address" env-default:"localhost:8080"`
+	Timeout     time.Duration `yaml:"timeout" env-default:"4s"`
 	IdleTimeout time.Duration `yaml:"idle-timeout" env-default:"60s"`
 }
 
 func MustLoad() *Config {
-	configPath := os.Getenv("CONFIG_PATH")
+	dir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	configPath := filepath.Join(dir, "config", "local.yaml")
 	if configPath == "" {
 		log.Fatal("CONFIG_PATH is not set")
 	}
@@ -29,11 +36,11 @@ func MustLoad() *Config {
 		log.Fatalf("Config file does not exist: %s", configPath)
 	}
 
-var cfg Config
+	var cfg Config
 
-if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
-	log.Fatalf("Cannot read config: %s", err)
-}
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("Cannot read config: %s", err)
+	}
 
-return &cfg
+	return &cfg
 }
